72a98f043095b56748875f1f9c4768fa0752c752
Juan Fernando Domínguez <juanfernando@MacBook-Pro-de-Juan.local>
2018-05-28 18:04:33 -0500

    Album card

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,62 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
+
   return (
-    <View>
-      <Text>{ props.album.title }</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{ title }</Text>
+          <Text>{ artist }</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image source={{ uri: image }} style={imageStyle}/>
+      </CardSection>
+    </Card>
   );
-}
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  imageStyle: {
+    height: 300,
+    //Full width
+    flex: 1,
+    width: null
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
+};
 
 export default AlbumDetail;
